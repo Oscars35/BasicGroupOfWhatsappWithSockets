dfbaa00de2d6b5abfce3fc26aacfe78c5b46c6ef
Oscars35 <oscar13785@gmail.com>
2021-02-11 13:31:33 +0100

    BasicWhatsappGroup

---
+++ BasicWhatsappGroup/whats-client.c
@@ -0,0 +1,81 @@
+#include<stdio.h>	//printf
+#include<string.h> //memset
+#include<stdlib.h> //exit(0);
+#include<stdbool.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+//Variables globals per a que quedi més bonic
+#define DEFAULT_PORT 1234
+#define DEFAULT_HOST "127.0.0.1"
+#define BUFFER_SIZE 1024
+char *CONNECTING_MESSAGE = "1";
+
+void die(char *s) 
+{
+	perror(s);
+	exit(1);
+}
+
+int main(void) 
+{
+	struct sockaddr_in si_other; //Struct of sockets
+	int s, i, slen=sizeof(si_other);
+	char buf[BUFFER_SIZE];
+	char message[BUFFER_SIZE];
+void die(char *s)  
+{
+        perror(s);
+        exit(1);
+}
+	
+	//If creation of sockets fails
+	if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		die("socket");
+	}
+
+	memset((char *) &si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(DEFAULT_PORT);
+	
+	// If is an error copiing to sin_addr, show 
+	if (inet_aton(DEFAULT_HOST , &si_other.sin_addr) == 0) 
+	{
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+
+	if (sendto(s, CONNECTING_MESSAGE, strlen(CONNECTING_MESSAGE) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        {
+        	die("sendto()");
+        }
+
+
+	while(1)
+	{
+		//receive a reply and print it
+    //clear the buffer by filling null, it might have previously received data
+    memset(buf,'\0', BUFFER_SIZE);
+    //try to receive some data, this is a blocking call
+    int n = recvfrom(s, buf, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *) &si_other, &slen) == -1;
+    if (n == -1)
+    {
+      die("recvfrom()");
+    }
+    
+    printf("Message arrived from another person: %s \n", buf);
+
+		printf("Enter message to send : ");
+		gets(message);
+		
+		//send the message
+		if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+		{
+			die("sendto()");
+		}
+		
+	}
+	close(s);
+	return 0;
+}
---
+++ BasicWhatsappGroup/whats-server.py
@@ -0,0 +1,63 @@
+import optparse
+import sys
+import socket
+
+#Variables globals per a que quedi més bonic
+DEFAULT_PORT = 1234
+DEFAULT_VERBOSE = False
+DEFAULT_HOST = '127.0.0.1'
+BUFFER_SIZE = 1024
+
+#List of clients
+clients = []
+host = DEFAULT_HOST
+port = DEFAULT_PORT
+verbose = False
+
+def parser():
+    global verbose, port, host  #es com el this per a la funció i detectar que agafem les de #listofclients
+    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter())
+    parser.add_option ('-v', '--verbose', action='store_true', default=DEFAULT_VERBOSE, help='verbose output')
+    parser.add_option ('--host', action='store', type='string', default=DEFAULT_HOST, help='Host, default LocalHost')
+    parser.add_option ('-p', '--port', action='store', type='int', default=DEFAULT_PORT, help='Listening port, default 1234')
+    (options, args) = parser.parse_args()
+    if len(args) > 0:
+        parser.error('bad args, use --help for help')
+    if options.port is not None:
+        port = options.port
+    if options.verbose is not False:
+        verbose = True
+    if options.host is not None:
+        host = options.host
+
+def get_new_socket():
+    whats_socket=socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    whats_socket.bind((host, port))
+    return whats_socket
+
+if __name__ == "__main__":
+    parser()
+    whats_socket = get_new_socket()
+    while True:
+        bytesAdressPair = whats_socket.recvfrom(BUFFER_SIZE)
+        message = bytesAdressPair[0]
+        address = bytesAdressPair[1]
+        message = message.decode("utf-8")
+        print (message)
+        print (address)
+        if message == '1':
+            message = str.encode(message)
+            whats_socket.sendto(message, address)
+            clients.append(address)
+        else:
+            for address_client in clients:
+                if address_client != address:
+                    message_to_send = str.encode(message)
+                    whats_socket.sendto(message_to_send, address_client)
+
+
+
+
+
+
+
