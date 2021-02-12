478ba595a3826d330de003d58eb79616ae7d94a2
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-02-12 18:44:42 +0100

    First changes for TCP

--- BasicWhatsappGroup/whats-server.py
+++ BasicWhatsappGroup/whats-server.py
@@ -1,3 +1,4 @@
+from multiprocessing import Queue, Process
 import optparse
 import sys
 import socket
@@ -31,29 +32,34 @@ def parser():
         host = options.host
 
 def get_new_socket():
-    whats_socket=socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    whats_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     whats_socket.bind((host, port))
     return whats_socket
 
+def read_from_socket(socket_client, queue, address):
+    while True:
+        message = socket_client.recv(BUFFER_SIZE)
+        if len(message) <= 0:
+            break
+        for client in queue:
+            if client != socket:
+                final_message = address + '-->' + message
+                client.send(str.encode(final_message))
+    socket_client.close()
+    sys.exit() 
+
 if __name__ == "__main__":
     parser()
+    clients_queue = Queue()
     whats_socket = get_new_socket()
+    whats_socket.listen(1)
     while True:
-        message, address = whats_socket.recvfrom(BUFFER_SIZE)
-        message = message.decode("utf-8")
-        print (message)
-        print (address)
-        if message == '1':
-            message = str.encode(message)
-            whats_socket.sendto(message, address)
-            clients.append(address)
-        else:
-            for address_client in clients:
-                if address_client != address:
-                    message_to_send = str.encode(message)
-                    whats_socket.sendto(message_to_send, address_client)
-
-
-
-
-
+        print("Waiting for client connections.....")
+        client_socket, address = whats_socket.accept()
+        print(f"{address} connected!")
+        # Add client to clients queue (we use queue because they are thread and process safe)
+        clients_queue.put(client_socket)
+        client_socket.send(str.encode("Connected to the server!"))
+        # Create process for each child and wait for a message
+        p = Process(target=read_from_socket, args=(client_socket, clients_queue, address))
+        p.start()
