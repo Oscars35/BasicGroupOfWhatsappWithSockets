a58b70ca699b941f652fafb1d23726d3edc1c7b1
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-02-14 13:17:14 +0100

    Server: Don't use signals, handle it with exceptions
    It seems way better than signals, since one are async and exceptions are
    easier to handle

--- BasicWhatsappGroup/whats-server.py
+++ BasicWhatsappGroup/whats-server.py
@@ -1,19 +1,23 @@
-import threading, optparse, sys, socket, signal
+import threading, optparse, sys, socket 
 from queue import Queue
 from FlagsParser import Parser
 from MessageFormater import *
 
+
 BUFFER_SIZE = 1024
 
-# Handle the sigint signal
-def signal_handler(sig, frame):
-    decision = input ("\nAre you sure you want to close the server?. y|n: ")
+
+# Closing menu
+def show_close_menu(clients, whats_socket, color_queue):
+    decision = input("\nAre you sure you want to close the server?. y|n: ")
     while decision != 'y' and decision != 'n' :
-        decision = input ("Bad output. Are you sure you want to close the server?. y|n: ")
+        decision = input("Bad output. Are you sure you want to close the server?. y|n: ")
     if decision == 'y':
         print('Okey!, Closing server!...')
         sys.exit()
-    signal.signal(signal.SIGINT, signal_handler)
+    connect_clients(clients, whats_socket, color_queue)
+
+
 
 # returns a color queue
 def get_color_queue():
@@ -23,11 +27,14 @@ def get_color_queue():
     return queue
 
 
+
 # Returns a TCP socket
 def get_socket(host, port):
     whats_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     whats_socket.bind((host, port))
     return whats_socket
+
+
 
 # Reads the message from the client. If the client has closed the socket, send to the other clients that the 
 # client has left the group and close his thread and socket
@@ -44,11 +51,15 @@ def receive_message(socket_client, clients, address, color_queue, color):
         socket_client.close()
         sys.exit() 
 
+
+
 # Sends the message to the receivers
 def send_message_other_clients(message, socket_sender, clients):
     for current_socket in clients:
         if current_socket != socket_sender:
             current_socket.send(str.encode(message))
+
+
 
 # Reads a message from a socket
 def read_from_socket(socket_client, clients, address, color_queue, color):
@@ -59,25 +70,41 @@ def read_from_socket(socket_client, clients, address, color_queue, color):
             print("socket error: %s",msg)
             sys.exit() 
 
+
+
+# Gets the connected client, it assigns him a color and reads from his socket
+def connect_client(clients, whats_socket, color_queue):
+        while True:
+            client_socket, address = whats_socket.accept()
+    
+            # Tell to everybody that the client joined the server
+            client_socket.send(str.encode(Formater.connection_stablished_client()))
+            send_message_other_clients(Formater.joined(address), client_socket, clients)
+            print(Formater.connection_stablished_server(address))
+    
+            # Get color for this user
+            color = color_queue.get()
+    
+            clients.append(client_socket)
+            tn = threading.Thread(target=read_from_socket, args=(client_socket, clients, address, color_queue, color), daemon=True)
+            tn.start()
+
+
+
+# Tries to get the clients. If the user does ctrl+c shows the menu for closing the server
+def connect_clients(clients, whats_socket, color_queue):
+    try:
+        connect_client(clients, whats_socket, color_queue)
+    except KeyboardInterrupt:
+        show_close_menu(clients, whats_socket, color_queue)
+
+
+# Main
 if __name__ == "__main__":
     host, port, verbose = Parser.parse()
     color_queue = get_color_queue()
     whats_socket = get_socket(host, port)
     whats_socket.listen(1)
-    signal.signal(signal.SIGINT, signal_handler)
-    clients = []
     print("Waiting for client connections.....")
-    while True:
-        client_socket, address = whats_socket.accept()
-
-        # Tell to everybody that the client joined the server
-        client_socket.send(str.encode(Formater.connection_stablished_client()))
-        send_message_other_clients(Formater.joined(address), client_socket, clients)
-        print(Formater.connection_stablished_server(address))
-
-        # Get color for this user
-        color = color_queue.get()
-
-        clients.append(client_socket)
-        tn = threading.Thread(target=read_from_socket, args=(client_socket, clients, address, color_queue, color), daemon=True)
-        tn.start()
+    connect_clients([], whats_socket, color_queue)
+ 
