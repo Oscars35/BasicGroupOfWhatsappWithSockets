ae1a85b1d2ccc242bc02fb9e5f034bcff41afed4
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-02-16 12:16:22 +0100

    client: use threads instead of processes

--- client/whats-client.c
+++ client/whats-client.c
@@ -6,6 +6,7 @@
 #include<sys/socket.h>
 #include<signal.h>
 #include<unistd.h>
+#include<pthread.h>
 
 #define DEFAULT_PORT 1234
 #define DEFAULT_HOST "127.0.0.1"
@@ -15,30 +16,31 @@
 #define COLOR_END "\033[00m"
 
 struct sockaddr_in socketStructure;
-int clientSocket, childPid, slen=sizeof(socketStructure);
+int clientSocket, slen=sizeof(socketStructure);
+pthread_t inputThread;
+pthread_t serverThread;
 
 void die(char *s);
 void initializeSocketConfiguration();
-void connectServer();
+void connecserverThread();
 void readInfoAndSendInfo();
-void readFromServer();
-void readFromKeyboardAndSendServer();
+void* readFromServer(void* arg);
+void* readFromKeyboardAndSendServer(void* arg);
 void exitMenuHandler(int sig);
 void sendNickNameToServer();
+void clearInputBuffer();
 
 
-int main(void) 
-{
+int main(void) {
     initializeSocketConfiguration();
-    connectServer();
+    connecserverThread();
     sendNickNameToServer();
     readInfoAndSendInfo();
 	close(clientSocket);
 	return 0;
 }
 
-void die(char *s) 
-{
+void die(char *s) {
 	perror(s);
 	exit(1);
 }
@@ -46,27 +48,28 @@ void die(char *s)
 void initializeSocketConfiguration() {
 	if ( (clientSocket=socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		die("socket()");
-    bzero(&socketStructure, sizeof(socketStructure));
+    memset(&socketStructure, 0, sizeof(socketStructure));
     socketStructure.sin_family = AF_INET; 
     socketStructure.sin_addr.s_addr = inet_addr(DEFAULT_HOST); 
     socketStructure.sin_port = htons(DEFAULT_PORT);
 }
 
-void connectServer() {
+void connecserverThread() {
     if(connect(clientSocket, (struct sockaddr *) &socketStructure, sizeof(socketStructure)) < 0)
         die("Error connect()");
 }
 
 void readInfoAndSendInfo() {
-    childPid = fork();
-    if (childPid == 0) {
-        readFromServer();
-    }else{
-        if (signal(SIGINT, exitMenuHandler) == SIG_ERR)
-            perror("Error initializing SIGINT");
-        readFromKeyboardAndSendServer();
-    }
-
+    if (signal(SIGINT, exitMenuHandler) == SIG_ERR)
+        perror("Error initializing SIGINT");
+    if (pthread_create(&serverThread, NULL, &readFromServer, NULL) != 0)
+        perror("Error creating thread reads from server");
+    if (pthread_create(&inputThread, NULL, &readFromKeyboardAndSendServer, NULL) != 0)
+        perror("Error creating thread reads from keyboard");
+    if (pthread_join(serverThread, NULL) != 0)
+        perror("Error Waiting thread read from server");
+    if (pthread_join(inputThread, NULL) != 0)
+        perror("Error waiting thread read from keyboard");
 }
 
 void sendNickNameToServer() {
@@ -77,43 +80,47 @@ void sendNickNameToServer() {
         die("sendto()");
 }
 
-void readFromServer() {
+void* readFromServer(void* arg) {
 	char buf[BUFFER_SIZE];
 	while(true)
 	{
         memset(buf,'\0', BUFFER_SIZE); 
         if (recv(clientSocket, buf, sizeof(buf), 0) <= 0) {
-            kill(getppid(), SIGKILL);
             die("Error reciving from server: Recv()");
         }
         printf("\n%s\n", buf);
 	}
 }
 
-void readFromKeyboardAndSendServer(){
+void* readFromKeyboardAndSendServer(void* arg){
 	char message[BUFFER_SIZE];
     while(true)
     {
         memset(message, '\0', BUFFER_SIZE);
         fgets(message, sizeof(message), stdin);
         if (sendto(clientSocket, message, strlen(message) , 0 , (struct sockaddr *) &socketStructure, slen) < 0)
-			      die("sendto()");
+		    die("sendto()");
     }
 }
 
 // Handle ctrl + c exit
 void exitMenuHandler(int sig) {
-            printf("%s\nAre you sure to exit? y|n %s \n", COLOR_RED, COLOR_END);
+        pthread_cancel(serverThread);
+        pthread_cancel(inputThread);
+        printf("%s\nAre you sure to exit? y|n %s \n", COLOR_RED, COLOR_END);
         char option;
         do {
             option = getchar();
         } while(option != 'y' && option != 'n');
-        if (option == 'y') {
-            kill(childPid, SIGKILL);
+        clearInputBuffer();
+        if (option == 'y')
             exit(0);
-        }
         if (signal(SIGINT, exitMenuHandler) == SIG_ERR)
             perror("Error initializing SIGUSR1");
+        readInfoAndSendInfo();
 }
 
-
+void clearInputBuffer() {
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != EOF);
+}
